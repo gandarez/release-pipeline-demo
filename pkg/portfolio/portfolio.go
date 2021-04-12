e085f6f645fc9dd7f6ac5524751932614827676c
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2021-04-12 14:15:08 -0300

    Fix memory leak

--- pkg/portfolio/portfolio.go
+++ pkg/portfolio/portfolio.go
@@ -6,6 +6,8 @@ func GetPortfolio(id int) (string, error) {
 	switch id {
 	case 1:
 		return "My Portfolio", nil
+	case 2:
+		return "My Managed Portfolio", nil
 	default:
 		return "", errors.New("id not found")
 	}
